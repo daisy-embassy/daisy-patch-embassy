511018d9d8bb31065a9fc2da427d8bb21400e8f5
Dickless <011001nobuo@gmail.com>
2025-05-10 21:35:04 +0900

    fixed variable name typo

--- src/oled.rs
+++ src/oled.rs
@@ -17,9 +17,9 @@ pub async fn oled_task(
 ) {
     let spi = embedded_hal_bus::spi::ExclusiveDevice::new_no_delay(spi, cs).unwrap();
     let interface = SPIInterface::new(spi, dc);
-    let mut display_i2c = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
+    let mut display_spi = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
-    display_i2c
+    display_spi
         .reset(&mut rst, &mut embassy_time::Delay {})
         .await
         .expect("reset failed");
@@ -33,5 +33,5 @@ pub async fn oled_task(
     let text_h = text_style.font.character_size.height as i32;
 
     let _ = Text::with_baseline("Hello!!", Point::new(0, 0), text_style, Baseline::Top)
-        .draw(&mut display_i2c);
+        .draw(&mut display_spi);
 }
