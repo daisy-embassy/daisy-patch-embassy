71186250f1bb061ab4c2df2783df3169b81d4c55
Dickless <011001nobuo@gmail.com>
2025-05-11 10:05:45 +0900

    Add debug logging to OLED task for better traceability

--- src/oled.rs
+++ src/oled.rs
@@ -1,4 +1,5 @@
-use embassy_stm32::{gpio::Output, mode::Async, spi::Spi};
+use defmt::debug;
+use embassy_stm32::gpio::Output;
 use embedded_graphics::Drawable;
 use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
@@ -12,13 +13,16 @@ use crate::dma_spi::DmaSpi;
 
 #[embassy_executor::task]
 pub async fn oled_task(spi: DmaSpi<'static>, dc: Output<'static>, mut rst: Output<'static>) {
+    debug!("oled_task");
     let interface = SPIInterface::new(spi, dc);
     let mut display_spi = Ssd1306Async::new(interface, DisplaySize128x64, DisplayRotation::Rotate0)
         .into_buffered_graphics_mode();
+    debug!("reset display");
     display_spi
         .reset(&mut rst, &mut embassy_time::Delay {})
         .await
         .expect("reset failed");
+    debug!("init display");
     display_spi.init().await.unwrap();
     let fc = BinaryColor::On;
     let bg = BinaryColor::Off;
@@ -36,5 +40,7 @@ pub async fn oled_task(spi: DmaSpi<'static>, dc: Output<'static>, mut rst: Outpu
     )
     .draw(&mut display_spi);
 
+    debug!("flush display");
     display_spi.flush().await.unwrap();
+    debug!("oled_task done");
 }
